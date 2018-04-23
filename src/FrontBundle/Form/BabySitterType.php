e258ebc34a19a5d82bbf74934398666dc1c316fd
YOSRABENAOUIENE <ben.aouiene.yosra@gmail.com>
2018-04-23 20:30:45 +0100

    crud front

---
+++ src/FrontBundle/Form/BabySitterType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace FrontBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BabySitterType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('birthDay')
+            ->add('description')
+            ->add('photo')
+            ->add('experience')
+            ->add('Rating', EntityType::class, array(
+                'class' => 'AppBundle\Entity\Rating',
+                'choice_label' => 'description',
+                'placeholder' => 'Please choose',
+                'empty_data' => null,
+                'required' => false
+ 
+            )) 
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\BabySitter'
+        ));
+    }
+}
