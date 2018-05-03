426034c734af3c55ff6921576d23fff0a6ea06a9
dali gharbi <shanks.dali@gmail.com>
2018-05-03 19:00:13 +0100

    webservice get all pediatres

--- src/FrontBundle/Controller/PediatricianController.php
+++ src/FrontBundle/Controller/PediatricianController.php
@@ -2,10 +2,13 @@
 
 namespace FrontBundle\Controller;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 /**
  * Event controller.
@@ -28,5 +31,20 @@ class PediatricianController extends Controller
         return $this->render('@Front/Pediatrician/index.html.twig', array(
             'pedaitricians'=>$pedaitricians
         ));
+    }
+
+    /**
+     * Lists all pediatrician entities.
+     *
+     * @Route("/all", name="front_list")
+     * @Method("GET")
+     */
+    public function List()
+    {
+        $em=$this->getDoctrine()->getManager();
+        $pedaitricians =$em->getRepository('AppBundle:Pediatrician')->findAll();
+        $serializer = new Serializer([new ObjectNormalizer()]);
+        $formatted = $serializer->normalize($pedaitricians);
+        return new JsonResponse($formatted);
     }
 }
