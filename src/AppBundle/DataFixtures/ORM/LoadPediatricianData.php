d099c06ee3c78782ef7c58761a2651be058dbf74
Mohamed Ali <g.mhdali@french-co.fr>
2018-06-25 10:09:45 +0100

    search ajax like marwen

--- src/AppBundle/DataFixtures/ORM/LoadPediatricianData.php
+++ src/AppBundle/DataFixtures/ORM/LoadPediatricianData.php
@@ -47,6 +47,23 @@ class LoadPediatricianData
         $manager->persist($pediatre);
 
 
+        $pediatre2 = new Pediatrician();
+        $pediatre2->setName('Dr Mostfa Hamdi 2');
+        $pediatre2->setEmail('shanks.dalii@gmail.com');
+        $adrs2 = new Address();
+        $adrs2->setRue('test 2');
+        $adrs2->setLongitude('36.8179854');
+        $adrs2->setLatitude('10.1946025');
+        $adrs2->setGovernorate('tunis');
+        $pediatre2->setAddress($adrs2);
+        $pediatre2->setPrice(45);
+        $pediatre2->setTel(27429028);
+        $rating2 = $manager->getRepository(Rating::class)->findOneBy(array('rate'=> 3));
+        $pediatre2->setRating($rating2);
+        $speciality2 = $manager->getRepository(Speciality::class)->findOneBy(array('name'=> 'Pediatric Cardiology'));
+        $pediatre2->setSpeciality($speciality2);
+        $manager->persist($pediatre2);
+
         $manager->flush();
     }
 
